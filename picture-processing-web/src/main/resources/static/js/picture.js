cd87d47f1f2d196f0ad9f535008c5dfb1cee86f4
shazhibin <shazhibin07@163.com>
2021-09-02 17:31:12 +0800

    添加图片调整

--- picture-processing-web/src/main/resources/static/js/picture.js
+++ picture-processing-web/src/main/resources/static/js/picture.js
@@ -26,17 +26,55 @@
     let canvas = document.createElement('canvas');
     canvas.width = 200;
     canvas.height = 200;
+    let context = canvas.getContext("2d");
+    let image2 = document.getElementById("image2");
+    let x = 0;
+    let y = 0;
+    let dw = 200;
+    let dh = 200;
 
     function bthClick(bth) {
-
-        let image1 = document.getElementById("image2");
-        let image = new Image(200, 200);
-        image.src = image1.src;
-        console.log(image)
-        let context = canvas.getContext("2d");
         context.fillStyle = bth.value;
         context.fillRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(image, 0, 0, 200, 200);
+        drawImage();
+    }
+
+    function drawImage() {
+        let image = new Image(dw, dh);
+        image.src = image2.src;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(image, x, y, dw, dh);
         document.querySelector("#image3").setAttribute("src", canvas.toDataURL());
+    }
+
+    let mvTid;
+    let chgTid;
+
+    function movePhoto(a, b) {
+        x = x + a;
+        y = y + b;
+        drawImage();
+    }
+
+    function btnMove(a, b) {
+        mvTid = setInterval(function () {
+            movePhoto(a, b)
+        }, 50)
+    }
+
+    function btnUp() {
+        clearInterval(mvTid);
+    }
+
+    function btnPhotoChange(a) {
+        chgTid = setInterval(function () {
+            dw = dw + a;
+            dh = dh + a;
+            drawImage();
+        }, 50)
+    }
+
+    function ChgBtnUp() {
+        clearInterval(chgTid);
     }
 }
